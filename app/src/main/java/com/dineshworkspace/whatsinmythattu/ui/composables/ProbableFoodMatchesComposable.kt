302189933b2a03e7ba2ad040c197520463e96cf6
Dinesh <dineshkarthie@gmail.com>
2024-05-17 08:59:37 +0530

    Displaying food match probability score in UI

--- app/src/main/java/com/dineshworkspace/whatsinmythattu/ui/composables/ProbableFoodMatchesComposable.kt
+++ app/src/main/java/com/dineshworkspace/whatsinmythattu/ui/composables/ProbableFoodMatchesComposable.kt
@@ -1,7 +1,10 @@
 package com.dineshworkspace.whatsinmythattu.ui.composables
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
@@ -11,7 +14,9 @@ import androidx.compose.ui.unit.dp
 import com.dineshworkspace.tensorimageinterpreter.ProbableFoodMatch
 
 @Composable
-fun ProbableFoodMatchesComposable(foodMatches: List<ProbableFoodMatch>) {
+fun ProbableFoodMatchesComposable(
+    foodMatches: List<ProbableFoodMatch>
+) {
     LazyColumn(
         modifier = Modifier.fillMaxWidth(),
         contentPadding = PaddingValues(16.dp)
@@ -24,5 +29,9 @@ fun ProbableFoodMatchesComposable(foodMatches: List<ProbableFoodMatch>) {
 
 @Composable
 fun ProbableMatchFoodItem(probableFoodMatch: ProbableFoodMatch) {
-    Text(text = probableFoodMatch.label)
+    Row {
+        Text(text = probableFoodMatch.label)
+        Spacer(modifier = Modifier.width(10.dp))
+        Text(text = probableFoodMatch.score.toString())
+    }
 }
