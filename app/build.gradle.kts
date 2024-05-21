5a5136cf67e14661e12e1ac9e65237e79ba6fc7e
Dinesh <dineshkarthie@gmail.com>
2024-05-21 07:45:28 +0530

    Version bump for stable release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.dineshworkspace.whatsinmythattu"
         minSdk = 33
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
